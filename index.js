6b46bd679516208cfc842b3b242ad12c42724e85
Bogdan <>
2020-07-27 17:03:02 +0300

    exporting test

---
+++ index.js
@@ -0,0 +1,3 @@
+import Test from './src/Test';
+
+export default Test;
---
+++ src/Test.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+import {Text} from 'react-native';
+
+export default class Test extends Component {
+  render() {
+    return <Text>{this.props.test}</Text>;
+  }
+}

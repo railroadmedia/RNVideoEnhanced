d0f35968876ef4dfd962bc8a5d4d69a4613e529b
Bogdan <>
2020-07-29 16:10:06 +0300

    * added formatTime function in VideoTimer

--- src/VideoTimer.js
+++ src/VideoTimer.js
@@ -12,13 +12,26 @@ export default class VideoTimer_V2 extends React.PureComponent {
     this.state.progress = '0:00';
   }
 
+  formatTime(seconds) {
+    if (seconds < 1) return '0:00';
+    let h = parseInt(seconds / 3600);
+    let m = parseInt((seconds - h * 3600) / 60);
+    let s = parseInt(seconds - m * 60 - h * 3600);
+
+    s = s < 10 ? `0${s}` : `${s}`;
+    m = m < 10 ? (h ? `0${m}` : `${m}`) : `${m}`;
+    return h ? `${h}:${m}:${s}` : `${m}:${s}`;
+  }
+
   setProgress = sec => {
-    this.setState({ progress: this.props.formatTime(sec) });
+    this.setState({ progress: this.formatTime(sec) });
   };
 
   render() {
-    let { progress } = this.state;
-    let { lengthInSec, formatTime } = this.props;
+    let {
+      state: { progress },
+      props: { lengthInSec }
+    } = this;
     return (
       <React.Fragment>
         <Text
@@ -32,7 +45,7 @@ export default class VideoTimer_V2 extends React.PureComponent {
           maxFontSizeMultiplier={this.props.maxFontMultiplier}
           style={{ color: 'white', padding: 10 }}
         >
-          {formatTime(lengthInSec)}
+          {this.formatTime(lengthInSec)}
         </Text>
       </React.Fragment>
     );

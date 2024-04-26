707e83fe92fc7ef586d50a59548b8472fc57d965
MeenuDhanoa <75337645+MeenuDhanoa@users.noreply.github.com>
2024-04-26 13:58:43 -0700

    Revert "MT-848: Migrate RNVideoEnhanced package to Typescript & Function Components" (#61)

--- src/img/ISvg.ts
+++ src/img/ISvg.ts
@@ -1,9 +1,6 @@
-import type { ViewStyle } from 'react-native';
-
 export default interface ISvg {
   height?: number;
   width?: number;
   fill?: string;
   active?: boolean;
-  style?: ViewStyle;
 }

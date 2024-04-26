4e80cddec999b3d6bd8a9a7d851df3f053f34004
MeenuDhanoa <75337645+MeenuDhanoa@users.noreply.github.com>
2024-04-26 13:55:06 -0700

    Revert "MT-848: Migrate RNVideoEnhanced package to Typescript & Function Components"

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ export const videoMock = {
   comments: [
     {
       id: 137416,
-      comment: 'It’s interesting that his drums are faced away from him.? Why is that??',
+      comment:
+        'It’s interesting that his drums are faced away from him.? Why is that??',
       content_id: 263378,
       parent_id: null,
       user_id: 367448,
@@ -34,18 +35,19 @@ export const videoMock = {
             'fields.profile_picture_image_url':
               'https://dzryyo1we6bm3.cloudfront.net/avatars/154138_1581108109625.jpg',
             level_number: '1.0',
-            isAdmin: true,
-          },
-        },
+            isAdmin: true
+          }
+        }
       ],
       like_count: 1,
       like_users: [
         {
           id: 154138,
           display_name: 'K-RAD',
-          avatar_url: 'https://dzryyo1we6bm3.cloudfront.net/avatars/154138_1581108109625.jpg',
-          xp: 0,
-        },
+          avatar_url:
+            'https://dzryyo1we6bm3.cloudfront.net/avatars/154138_1581108109625.jpg',
+          xp: 0
+        }
       ],
       is_liked: false,
       user: {
@@ -57,9 +59,9 @@ export const videoMock = {
         xp_level: 'Enthusiast I',
         access_level: 'lifetime',
         level_number: '1.0',
-        isAdmin: false,
-      },
-    },
+        isAdmin: false
+      }
+    }
   ],
   id: 263378,
   xp: 200,
@@ -86,14 +88,16 @@ export const videoMock = {
       name: 'Daru Jones',
       biography:
         'Daru Jones is a Grammy-award winner with an impressive level of experience performing and recording, and producing at the highest level through his own label: Rusic Records. He has worked with incredible artists and producers, such as Jack White, Slum Village, Talib Kweli, Black Milk, Dwight Yoakam, Jamie Lidell, Pete Rock, Salaam Remi, Gloria Gaynor, Kim Burrell, Q-Tip, Mos Def, and Lorenzo Jovanotti, just to name a few. This level of exposure has led him to play on coveted stages, including One Mic: Hip-Hop Culture Worldwide, the 2013 Grammy Awards, Austin City Limits, Rock The Bells Festival, Montreux Jazz Festival, The David Letterman Show, Jools Holland, The Colbert Report, Saturday Night Live, and MTV. ',
-      headShotPic: 'https://d1923uyy6spedc.cloudfront.net/220967-avatar-1572358845.jpg',
-    },
+      headShotPic:
+        'https://d1923uyy6spedc.cloudfront.net/220967-avatar-1572358845.jpg'
+    }
   ],
   description:
     'Daru explains his approach to playing drums, and how his style is identifiably unique. Soul Hop is his way of combining two of his biggest influences: soul music and hip-hop.',
   assignments: [],
   mp3s: [],
-  thumbnailUrl: 'https://d1923uyy6spedc.cloudfront.net/263378-card-thumbnail-1595685275.png',
+  thumbnailUrl:
+    'https://d1923uyy6spedc.cloudfront.net/263378-card-thumbnail-1595685275.png',
   nextLessonId: 263379,
   isYoutube: false,
   relatedLessons: [
@@ -108,8 +112,9 @@ export const videoMock = {
       style: '',
       title: 'Jack White - "Lazaretto"',
       artist: '',
-      thumbnailUrl: 'https://d1923uyy6spedc.cloudfront.net/263379-card-thumbnail-1595685452.png',
-      length_in_seconds: '201',
+      thumbnailUrl:
+        'https://d1923uyy6spedc.cloudfront.net/263379-card-thumbnail-1595685452.png',
+      length_in_seconds: '201'
     },
     {
       id: 263380,
@@ -122,8 +127,9 @@ export const videoMock = {
       style: '',
       title: 'The Ruff Pack - "With You"',
       artist: '',
-      thumbnailUrl: 'https://d1923uyy6spedc.cloudfront.net/263380-card-thumbnail-1595685571.png',
-      length_in_seconds: '179',
+      thumbnailUrl:
+        'https://d1923uyy6spedc.cloudfront.net/263380-card-thumbnail-1595685571.png',
+      length_in_seconds: '179'
     },
     {
       id: 263381,
@@ -136,8 +142,9 @@ export const videoMock = {
       style: '',
       title: 'Black Milk - "Keep Going"',
       artist: '',
-      thumbnailUrl: 'https://d1923uyy6spedc.cloudfront.net/263381-card-thumbnail-1595685676.png',
-      length_in_seconds: '443',
+      thumbnailUrl:
+        'https://d1923uyy6spedc.cloudfront.net/263381-card-thumbnail-1595685676.png',
+      length_in_seconds: '443'
     },
     {
       id: 263382,
@@ -150,8 +157,9 @@ export const videoMock = {
       style: '',
       title: 'Slum Village - "Just Like A Test"',
       artist: '',
-      thumbnailUrl: 'https://d1923uyy6spedc.cloudfront.net/263382-card-thumbnail-1595685733.png',
-      length_in_seconds: '241',
+      thumbnailUrl:
+        'https://d1923uyy6spedc.cloudfront.net/263382-card-thumbnail-1595685733.png',
+      length_in_seconds: '241'
     },
     {
       id: 263383,
@@ -164,50 +172,58 @@ export const videoMock = {
       style: '',
       title: 'Jamie Lidell - "Walk Right Back"',
       artist: '',
-      thumbnailUrl: 'https://d1923uyy6spedc.cloudfront.net/263383-card-thumbnail-1595685793.png',
-      length_in_seconds: '614',
-    },
+      thumbnailUrl:
+        'https://d1923uyy6spedc.cloudfront.net/263383-card-thumbnail-1595685793.png',
+      length_in_seconds: '614'
+    }
   ],
   isAdded: false,
   incompleteLessonId: 263379,
   video_playback_endpoints: [
     {
-      file: 'https://player.vimeo.com/external/440749961.sd.mp4?s=4ac4f77c50cb5f9994a544ffc75a93146c12b4c6&profile_id=139&oauth2_token_id=1284792283',
+      file:
+        'https://player.vimeo.com/external/440749961.sd.mp4?s=4ac4f77c50cb5f9994a544ffc75a93146c12b4c6&profile_id=139&oauth2_token_id=1284792283',
       width: 426,
-      height: 240,
+      height: 240
     },
     {
-      file: 'https://player.vimeo.com/external/440749961.sd.mp4?s=4ac4f77c50cb5f9994a544ffc75a93146c12b4c6&profile_id=164&oauth2_token_id=1284792283',
+      file:
+        'https://player.vimeo.com/external/440749961.sd.mp4?s=4ac4f77c50cb5f9994a544ffc75a93146c12b4c6&profile_id=164&oauth2_token_id=1284792283',
       width: 640,
-      height: 360,
+      height: 360
     },
     {
-      file: 'https://player.vimeo.com/external/440749961.sd.mp4?s=4ac4f77c50cb5f9994a544ffc75a93146c12b4c6&profile_id=165&oauth2_token_id=1284792283',
+      file:
+        'https://player.vimeo.com/external/440749961.sd.mp4?s=4ac4f77c50cb5f9994a544ffc75a93146c12b4c6&profile_id=165&oauth2_token_id=1284792283',
       width: 960,
-      height: 540,
+      height: 540
     },
     {
-      file: 'https://player.vimeo.com/external/440749961.hd.mp4?s=0bd3c88f773d4e2b10e74a34cbd52500f47fb678&profile_id=174&oauth2_token_id=1284792283',
+      file:
+        'https://player.vimeo.com/external/440749961.hd.mp4?s=0bd3c88f773d4e2b10e74a34cbd52500f47fb678&profile_id=174&oauth2_token_id=1284792283',
       width: 1280,
-      height: 720,
+      height: 720
     },
     {
-      file: 'https://player.vimeo.com/external/440749961.hd.mp4?s=0bd3c88f773d4e2b10e74a34cbd52500f47fb678&profile_id=175&oauth2_token_id=1284792283',
+      file:
+        'https://player.vimeo.com/external/440749961.hd.mp4?s=0bd3c88f773d4e2b10e74a34cbd52500f47fb678&profile_id=175&oauth2_token_id=1284792283',
       width: 1920,
-      height: 1080,
+      height: 1080
     },
     {
-      file: 'https://player.vimeo.com/external/440749961.hd.mp4?s=0bd3c88f773d4e2b10e74a34cbd52500f47fb678&profile_id=170&oauth2_token_id=1284792283',
+      file:
+        'https://player.vimeo.com/external/440749961.hd.mp4?s=0bd3c88f773d4e2b10e74a34cbd52500f47fb678&profile_id=170&oauth2_token_id=1284792283',
       width: 2560,
-      height: 1440,
+      height: 1440
     },
     {
-      file: 'https://player.vimeo.com/external/440749961.hd.mp4?s=0bd3c88f773d4e2b10e74a34cbd52500f47fb678&profile_id=172&oauth2_token_id=1284792283',
+      file:
+        'https://player.vimeo.com/external/440749961.hd.mp4?s=0bd3c88f773d4e2b10e74a34cbd52500f47fb678&profile_id=172&oauth2_token_id=1284792283',
       width: 3840,
-      height: 2160,
-    },
+      height: 2160
+    }
   ],
-  lastWatchedPosInSec: 0,
+  lastWatchedPosInSec: 0
 };
 export const mp3VideoMock = {
   comments: [
@@ -233,9 +249,9 @@ export const mp3VideoMock = {
         xp_level: 'Enthusiast I',
         access_level: 'lifetime',
         level_number: '1.0',
-        isAdmin: false,
-      },
-    },
+        isAdmin: false
+      }
+    }
   ],
   id: 245323,
   xp: 175,
@@ -255,13 +271,14 @@ export const mp3VideoMock = {
       resource_id: 123395,
       resource_name: 'MP3 Without Click',
       resource_url:
-        'https://s3.amazonaws.com/drumeo/play-along-resources/funk-you-not/funk-you-not-drums-false-click-false.mp3',
+        'https://s3.amazonaws.com/drumeo/play-along-resources/funk-you-not/funk-you-not-drums-false-click-false.mp3'
     },
     {
       resource_id: 123400,
       resource_name: 'PDF Chart',
-      resource_url: 'https://d1923uyy6spedc.cloudfront.net/245323-resource-1582899677.pdf',
-    },
+      resource_url:
+        'https://d1923uyy6spedc.cloudfront.net/245323-resource-1582899677.pdf'
+    }
   ],
   instructor: 'Daru Jones',
   difficulty: 'BEGINNER 4',
@@ -274,8 +291,9 @@ export const mp3VideoMock = {
       name: 'Daru Jones',
       biography:
         'Daru Jones is a Grammy-award winner with an impressive level of experience performing and recording, and producing at the highest level through his own label: Rusic Records. He has worked with incredible artists and producers, such as Jack White, Slum Village, Talib Kweli, Black Milk, Dwight Yoakam, Jamie Lidell, Pete Rock, Salaam Remi, Gloria Gaynor, Kim Burrell, Q-Tip, Mos Def, and Lorenzo Jovanotti, just to name a few. This level of exposure has led him to play on coveted stages, including One Mic: Hip-Hop Culture Worldwide, the 2013 Grammy Awards, Austin City Limits, Rock The Bells Festival, Montreux Jazz Festival, The David Letterman Show, Jools Holland, The Colbert Report, Saturday Night Live, and MTV. ',
-      headShotPic: 'https://d1923uyy6spedc.cloudfront.net/220967-avatar-1572358845.jpg',
-    },
+      headShotPic:
+        'https://d1923uyy6spedc.cloudfront.net/220967-avatar-1572358845.jpg'
+    }
   ],
   description: '',
   assignments: [
@@ -289,13 +307,14 @@ export const mp3VideoMock = {
       soundsliceSlug: '',
       sheets: [
         {
-          value: 'https://d1923uyy6spedc.cloudfront.net/246968-sheet-image-1582899718.svg',
+          value:
+            'https://d1923uyy6spedc.cloudfront.net/246968-sheet-image-1582899718.svg',
           id: 123402,
-          whRatio: 1.0132450331125828,
-        },
+          whRatio: 1.0132450331125828
+        }
       ],
-      timeCodes: [],
-    },
+      timeCodes: []
+    }
   ],
   mp3s: [
     {
@@ -304,7 +323,7 @@ export const mp3VideoMock = {
       key: 'mp3_no_drums_no_click_url',
       value:
         'https://s3.amazonaws.com/drumeo/play-along-resources/funk-you-not/funk-you-not-drums-false-click-false.mp3',
-      position: 1,
+      position: 1
     },
     {
       id: 123397,
@@ -312,7 +331,7 @@ export const mp3VideoMock = {
       key: 'mp3_no_drums_yes_click_url',
       value:
         'https://s3.amazonaws.com/drumeo/play-along-resources/funk-you-not/funk-you-not-drums-false-click-true.mp3',
-      position: 1,
+      position: 1
     },
     {
       id: 123398,
@@ -320,7 +339,7 @@ export const mp3VideoMock = {
       key: 'mp3_yes_drums_no_click_url',
       value:
         'https://s3.amazonaws.com/drumeo/play-along-resources/funk-you-not/funk-you-not-drums-true-click-false.mp3',
-      position: 1,
+      position: 1
     },
     {
       id: 123399,
@@ -328,10 +347,11 @@ export const mp3VideoMock = {
       key: 'mp3_yes_drums_yes_click_url',
       value:
         'https://s3.amazonaws.com/drumeo/play-along-resources/funk-you-not/funk-you-not-drums-true-click-true.mp3',
-      position: 1,
-    },
+      position: 1
+    }
   ],
-  thumbnailUrl: 'https://dzryyo1we6bm3.cloudfront.net/thumbnails/22911_thumbnail_360p.jpg',
+  thumbnailUrl:
+    'https://dzryyo1we6bm3.cloudfront.net/thumbnails/22911_thumbnail_360p.jpg',
   nextLessonId: 243773,
   isYoutube: false,
   soundsliceSlug: '',
@@ -347,8 +367,9 @@ export const mp3VideoMock = {
       style: 'Funk',
       title: 'Xeno Bounce',
       artist: '',
-      thumbnailUrl: 'https://d1923uyy6spedc.cloudfront.net/261676-card-thumbnail-1594994038.png',
-      length_in_seconds: '199',
+      thumbnailUrl:
+        'https://d1923uyy6spedc.cloudfront.net/261676-card-thumbnail-1594994038.png',
+      length_in_seconds: '199'
     },
     {
       id: 262303,
@@ -362,7 +383,7 @@ export const mp3VideoMock = {
       title: 'Seven Gateways Of Time',
       artist: '',
       thumbnailUrl: '',
-      length_in_seconds: '238',
+      length_in_seconds: '238'
     },
     {
       id: 260251,
@@ -375,8 +396,9 @@ export const mp3VideoMock = {
       style: 'jazz',
       title: 'Nutville',
       artist: 'Buddy Rich Big Band',
-      thumbnailUrl: 'https://d1923uyy6spedc.cloudfront.net/260251-card-thumbnail-1593035689.png',
-      length_in_seconds: '173',
+      thumbnailUrl:
+        'https://d1923uyy6spedc.cloudfront.net/260251-card-thumbnail-1593035689.png',
+      length_in_seconds: '173'
     },
     {
       id: 254192,
@@ -389,8 +411,9 @@ export const mp3VideoMock = {
       style: 'Funk',
       title: 'Black Cow',
       artist: 'Steely Dan',
-      thumbnailUrl: 'https://d1923uyy6spedc.cloudfront.net/254192-card-thumbnail-1593035708.png',
-      length_in_seconds: '294',
+      thumbnailUrl:
+        'https://d1923uyy6spedc.cloudfront.net/254192-card-thumbnail-1593035708.png',
+      length_in_seconds: '294'
     },
     {
       id: 254179,
@@ -403,8 +426,9 @@ export const mp3VideoMock = {
       style: 'Alternative',
       title: 'Speak To Me',
       artist: 'Rashid Williams',
-      thumbnailUrl: 'https://d1923uyy6spedc.cloudfront.net/254179-card-thumbnail-1593035722.png',
-      length_in_seconds: '249',
+      thumbnailUrl:
+        'https://d1923uyy6spedc.cloudfront.net/254179-card-thumbnail-1593035722.png',
+      length_in_seconds: '249'
     },
     {
       id: 254163,
@@ -417,8 +441,9 @@ export const mp3VideoMock = {
       style: 'Vocals',
       title: 'Blue Jacket',
       artist: 'Mathieu Fiset & The RoboJazz Band',
-      thumbnailUrl: 'https://d1923uyy6spedc.cloudfront.net/254163-card-thumbnail-1593035733.png',
-      length_in_seconds: '247',
+      thumbnailUrl:
+        'https://d1923uyy6spedc.cloudfront.net/254163-card-thumbnail-1593035733.png',
+      length_in_seconds: '247'
     },
     {
       id: 253432,
@@ -431,8 +456,9 @@ export const mp3VideoMock = {
       style: 'Funk',
       title: 'Get The Groove Right',
       artist: 'Mathieu Fiset & The RoboJazz Band',
-      thumbnailUrl: 'https://d1923uyy6spedc.cloudfront.net/253432-card-thumbnail-1593035744.png',
-      length_in_seconds: '203',
+      thumbnailUrl:
+        'https://d1923uyy6spedc.cloudfront.net/253432-card-thumbnail-1593035744.png',
+      length_in_seconds: '203'
     },
     {
       id: 248523,
@@ -445,8 +471,9 @@ export const mp3VideoMock = {
       style: 'Latin',
       title: 'Cha Cha 2',
       artist: 'Jody Fisher, Baba Elefante, Tilden Webb & Mike Michalkow',
-      thumbnailUrl: 'https://dzryyo1we6bm3.cloudfront.net/thumbnails/30368_thumbnail_360p.jpg',
-      length_in_seconds: '131',
+      thumbnailUrl:
+        'https://dzryyo1we6bm3.cloudfront.net/thumbnails/30368_thumbnail_360p.jpg',
+      length_in_seconds: '131'
     },
     {
       id: 247399,
@@ -459,8 +486,9 @@ export const mp3VideoMock = {
       style: 'Electronic',
       title: 'Futuristic Solitude',
       artist: 'Tim Proznick',
-      thumbnailUrl: 'https://dzryyo1we6bm3.cloudfront.net/thumbnails/22911_thumbnail_360p.jpg',
-      length_in_seconds: '215',
+      thumbnailUrl:
+        'https://dzryyo1we6bm3.cloudfront.net/thumbnails/22911_thumbnail_360p.jpg',
+      length_in_seconds: '215'
     },
     {
       id: 246167,
@@ -473,48 +501,56 @@ export const mp3VideoMock = {
       style: 'Electronic',
       title: 'The Odd Life',
       artist: 'Tim Proznick',
-      thumbnailUrl: 'https://dzryyo1we6bm3.cloudfront.net/thumbnails/22911_thumbnail_360p.jpg',
-      length_in_seconds: '244',
-    },
+      thumbnailUrl:
+        'https://dzryyo1we6bm3.cloudfront.net/thumbnails/22911_thumbnail_360p.jpg',
+      length_in_seconds: '244'
+    }
   ],
   isAdded: false,
   previousLessonId: 246167,
   video_playback_endpoints: [
     {
-      file: 'https://player.vimeo.com/external/393716651.sd.mp4?s=50f8575126b9a70e084931f6f9a717b9125fc22b&profile_id=139&oauth2_token_id=1284792283',
+      file:
+        'https://player.vimeo.com/external/393716651.sd.mp4?s=50f8575126b9a70e084931f6f9a717b9125fc22b&profile_id=139&oauth2_token_id=1284792283',
       width: 426,
-      height: 240,
+      height: 240
     },
     {
-      file: 'https://player.vimeo.com/external/393716651.sd.mp4?s=50f8575126b9a70e084931f6f9a717b9125fc22b&profile_id=164&oauth2_token_id=1284792283',
+      file:
+        'https://player.vimeo.com/external/393716651.sd.mp4?s=50f8575126b9a70e084931f6f9a717b9125fc22b&profile_id=164&oauth2_token_id=1284792283',
       width: 640,
-      height: 360,
+      height: 360
     },
     {
-      file: 'https://player.vimeo.com/external/393716651.sd.mp4?s=50f8575126b9a70e084931f6f9a717b9125fc22b&profile_id=165&oauth2_token_id=1284792283',
+      file:
+        'https://player.vimeo.com/external/393716651.sd.mp4?s=50f8575126b9a70e084931f6f9a717b9125fc22b&profile_id=165&oauth2_token_id=1284792283',
       width: 960,
-      height: 540,
+      height: 540
     },
     {
-      file: 'https://player.vimeo.com/external/393716651.hd.mp4?s=b4962b2c9c71d6e6bf22211195e6d94990eedb29&profile_id=174&oauth2_token_id=1284792283',
+      file:
+        'https://player.vimeo.com/external/393716651.hd.mp4?s=b4962b2c9c71d6e6bf22211195e6d94990eedb29&profile_id=174&oauth2_token_id=1284792283',
       width: 1280,
-      height: 720,
+      height: 720
     },
     {
-      file: 'https://player.vimeo.com/external/393716651.hd.mp4?s=b4962b2c9c71d6e6bf22211195e6d94990eedb29&profile_id=175&oauth2_token_id=1284792283',
+      file:
+        'https://player.vimeo.com/external/393716651.hd.mp4?s=b4962b2c9c71d6e6bf22211195e6d94990eedb29&profile_id=175&oauth2_token_id=1284792283',
       width: 1920,
-      height: 1080,
+      height: 1080
     },
     {
-      file: 'https://player.vimeo.com/external/393716651.hd.mp4?s=b4962b2c9c71d6e6bf22211195e6d94990eedb29&profile_id=170&oauth2_token_id=1284792283',
+      file:
+        'https://player.vimeo.com/external/393716651.hd.mp4?s=b4962b2c9c71d6e6bf22211195e6d94990eedb29&profile_id=170&oauth2_token_id=1284792283',
       width: 2560,
-      height: 1440,
+      height: 1440
     },
     {
-      file: 'https://player.vimeo.com/external/393716651.hd.mp4?s=b4962b2c9c71d6e6bf22211195e6d94990eedb29&profile_id=172&oauth2_token_id=1284792283',
+      file:
+        'https://player.vimeo.com/external/393716651.hd.mp4?s=b4962b2c9c71d6e6bf22211195e6d94990eedb29&profile_id=172&oauth2_token_id=1284792283',
       width: 3840,
-      height: 2160,
-    },
+      height: 2160
+    }
   ],
-  lastWatchedPosInSec: 0,
+  lastWatchedPosInSec: 0
 };
